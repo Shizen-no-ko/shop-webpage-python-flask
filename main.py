285988334605a754326703e22ffb2c98e7266a03
Mike Search <search_michael@yahoo.com>
2021-06-03 15:46:43 +0200

    Sets up basic html, css and routing. Imports trial fonts and fontawesome

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 from dotenv import load_dotenv
+from flask import Flask, render_template, redirect, url_for, flash
+from datetime import datetime
 
 load_dotenv()
 
+now = datetime.now()
+current_year = now.strftime("%Y")
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+	return render_template("index.html", current_year=current_year)
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
