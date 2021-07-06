86493cfe40a0af308639e55eb2ff2ce8838ad65d
Mike Search <search_michael@yahoo.com>
2021-07-06 16:15:38 +0200

    Comments script.js file

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+// toggle modal on or off
  function toggleModal() {
             backdrop.classList.toggle('show');
             modalCard.classList.toggle('show');
@@ -10,17 +11,24 @@ function setButtons(){
     modalCard = document.getElementById('modal-card');
     var i;
     for (i = 0; i < buttons.length; i++) {
+//        apply onclick function to artwork buttons to trigger modal
         buttons[i].onclick = function(){
+//        obtain id number from button id
             var id = this.id;
             var num = id.split('-')[1];
+//            grab infos for display in modal
             var titleText = document.getElementById('full-title-' + num).innerHTML;
             var descriptionText = document.getElementById('description-' + num).innerHTML;
             var priceText = document.getElementById('price-' + num).innerHTML;
             var imageUrl = document.getElementById('img-' + num).src;
+//            scroll to page top for displaying modal
             document.documentElement.scrollTop = 0;
+//            set modal close button to toggleModal function
             modalClose = document.getElementById('close-modal-button');
             modalClose.onclick = toggleModal;
+//            display modal
             toggleModal()
+//            set modal details to infos gathered above from artwork card
             document.getElementById('modal-title').innerHTML = titleText;
             document.getElementById('modal-image').src = imageUrl;
             document.getElementById('modal-description').innerHTML = descriptionText;
@@ -31,6 +39,7 @@ function setButtons(){
     };
 };
 
+//only display cart number if greater than 0
 function setCartNumber(){
     cartNumber = document.getElementById("cart-count");
     if (cartNumber.innerHTML != "0"){
@@ -41,6 +50,7 @@ function setCartNumber(){
 
 }
 
+//run after page has loaded
 window.onload = function(){
  setButtons()
  setCartNumber()
